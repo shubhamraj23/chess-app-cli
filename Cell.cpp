dd025a5b027b16557d3726bdee6b6b9c9a8a00a8
Shubham Raj Pandit <shubhamraj465@gmail.com>
2023-08-04 16:28:21 +0530

    Updated the Piece header file to include all declarations

--- Cell.cpp
+++ Cell.cpp
@@ -1,20 +1,22 @@
 #include "Cell.h"
 
-Cell::Cell() {
-  
-}
+// Default constructor for initialization.
+Cell::Cell() {}
 
+// Constructor for cells without a piece.
 Cell::Cell(int i, int j) {
   Cell::assignCellName(i, j);
   empty = true;
 }
 
+// Constructor for cells with a piece.
 Cell::Cell(int i, int j, Piece* p) {
   Cell::assignCellName(i, j);
   empty = false;
   piece = p;
 }
 
+// Function to assign the cell name to the cell.
 void Cell::assignCellName(int i, int j) {
   cellName = char(97 + i) + char(49 + j);
 }
--- Piece.h
+++ Piece.h
@@ -3,4 +3,52 @@
 class Piece {
   private:
     std::string colour;
+    std::string type;
+    bool alive;
+
+  public:
+    virtual bool isValid(std::string start, std::string end) = 0;
+    virtual void move(std::string start, std::string end) = 0;
+};
+
+class Pawn : public Piece {
+  public:
+    Pawn(std::string colour);
+    bool isValid(std::string start, std::string end) override;
+    void move(std::string start, std::string end) override;
+};
+
+class Rook : public Piece {
+  public:
+    Rook(std::string colour);
+    bool isValid(std::string start, std::string end) override;
+    void move(std::string start, std::string end) override;
+};
+
+class Knight : public Piece {
+  public:
+    Knight(std::string colour);
+    bool isValid(std::string start, std::string end) override;
+    void move(std::string start, std::string end) override;
+};
+
+class Bishop : public Piece {
+  public:
+    Bishop(std::string colour);
+    bool isValid(std::string start, std::string end) override;
+    void move(std::string start, std::string end) override;
+};
+
+class Queen : public Piece {
+  public:
+    Queen(std::string colour);
+    bool isValid(std::string start, std::string end) override;
+    void move(std::string start, std::string end) override;
+};
+
+class King : public Piece {
+  public:
+    King(std::string colour);
+    bool isValid(std::string start, std::string end) override;
+    void move(std::string start, std::string end) override;
 };
